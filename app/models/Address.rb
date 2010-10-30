8ae1818858b162f6a00d799291885816fa32d0f3
Todd Matthews <todd.matthews@razorfish.com>
2010-10-30 14:07:43 -0400

    Initial Load.

---
+++ app/models/Address.rb
@@ -0,0 +1,21 @@
+class Address < Hash
+  include ::CouchRest::CastedModel
+  
+  property :line_1
+  property :line_2
+  property :city
+  property :state
+  property :zip_code
+  property :country, :default => "USA"
+
+  def to_s
+    address_str = "#{line_1}"
+    address_str << ",\n #{line_2}" if line_2
+    address_str << "\n"
+    address_str << "#{city}, " if city
+    address_str << "#{state}" if state
+    address_str << " #{zip_code}" if zip_code
+    address_str << "\n#{country}" if country
+    address_str
+  end
+end
---
+++ app/models/Contact.rb
@@ -0,0 +1,21 @@
+require 'rubygems'
+require 'couchrest'
+
+SERVER = CouchRest.new
+DB     = SERVER.database!('skillsdatabase')
+
+class Contact < CouchRest::ExtendedDocument
+  use_database DB
+  
+  property :first_name
+  property :last_name, :alias => :family_name
+  property :company_name
+  property :job_title
+  property :address, :cast_as => 'Address'
+  property :phone_numbner
+  property :email
+  timestamps!
+  
+  view_by :first_name
+  
+end
