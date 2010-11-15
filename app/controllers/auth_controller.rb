5b0502894cb1f062b19a94cd0036a2dcb192eeeb
Todd Matthews <todd.matthews@razorfish.com>
2010-11-14 22:24:03 -0500

    removed hard-coded host and port

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -2,7 +2,7 @@ class AuthController < ApplicationController
   def index
 
     client = LinkedIn::Client.new('eA_Uz2aQ8XlzEmiQv5zEcvcM3HYm-PYUghlJTir3FSC9L_Wbq8At_9kSpnj1lZQx', 'g2n_LE5xH3Mr1Repvki_imIO7LqvrIfcqwCOCdQRxBJ3JTto434BC3SR8fgijIrT')
-    request_token = client.request_token(:oauth_callback =>"http://localhost:3000/auth/callback")
+    request_token = client.request_token(:oauth_callback =>"http://#{request.host_with_port}/auth/callback")
     session[:rtoken] = request_token.token
     session[:rsecret] = request_token.secret
     redirect_to client.request_token.authorize_url
