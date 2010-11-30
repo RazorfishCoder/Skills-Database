e98d69839ce05fd71f33f8d0229213565f5e3605
Todd Matthews <todd.matthews@razorfish.com>
2010-11-30 12:50:00 -0500

    removed unnecessary welcome code

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,21 +1,11 @@
 
 class WelcomeController < ApplicationController
   def index
-    #@linkedin = Linkedin.first
-
-    # get your api keys at https://www.linkedin.com/secure/developer
-    @client = LinkedIn::Client.new('eA_Uz2aQ8XlzEmiQv5zEcvcM3HYm-PYUghlJTir3FSC9L_Wbq8At_9kSpnj1lZQx', 'g2n_LE5xH3Mr1Repvki_imIO7LqvrIfcqwCOCdQRxBJ3JTto434BC3SR8fgijIrT')
-    @rtoken = @client.request_token.token
-    @rsecret = @client.request_token.secret
-
-    # then fetch your access keys
-    #@client.authorize_from_request(@rtoken, @rsecret)
-
-
-    #@myProfile = @client.profile.headline
-
 
   end
     
+  def test
+    
+  end
 
 end
