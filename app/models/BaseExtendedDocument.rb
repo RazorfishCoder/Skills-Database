5559bfe2815a47eb3940e3cfcad5154cfbc7f9d1
Todd Matthews <todd.matthews@razorfish.com>
2010-11-30 12:51:45 -0500

    Refactored & Removed

--- app/models/BaseExtendedDocument.rb
+++
@@ -1,9 +0,0 @@
-require 'rubygems'
-require 'couchrest'
-
-SERVER = CouchRest.new
-DB     = SERVER.database!('skillsdatabase')
-
-class BaseExtendedDocument < CouchRest::ExtendedDocument
-  use_database DB
-end
