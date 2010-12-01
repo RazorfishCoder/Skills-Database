35f98cf0d4b4d2aa99b7b1d9989def0ffbc6e076
Todd Matthews <todd.matthews@razorfish.com>
2010-12-01 10:38:23 -0500

    Added comments and validations

--- app/models/Linkedin.rb
+++ app/models/Linkedin.rb
@@ -2,7 +2,15 @@ require 'BaseCouchDocument'
 
 class Linkedin < BaseCouchDocument
   
+  #############
+  # Properties
+  #############
   property :api_key
   property :secret_key
+  
+  #############
+  # Validations
+  #############
+  validates_uniqueness_of :api_key
   
 end
