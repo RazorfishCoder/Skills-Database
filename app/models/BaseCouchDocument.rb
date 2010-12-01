da6a12e7ff5e8428b874543966478421fe300dfc
Todd Matthews <todd.matthews@razorfish.com>
2010-12-01 11:11:03 -0500

    Reconfigured to use initializer

--- app/models/BaseCouchDocument.rb
+++ app/models/BaseCouchDocument.rb
@@ -2,10 +2,6 @@
 #require 'rubygems'
 #require 'couchrest'
 
-# CouchDB Configuration
-SERVER = CouchRest.new
-DB     = SERVER.database!('skillsdatabase')
-
 class BaseCouchDocument < CouchRest::Model::Base
-  use_database DB
+  use_database CouchServer.default_database    #this is configured in the config/initializers/couchdb.rb
 end
