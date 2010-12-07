6b684eeff6cfe44ecaf2302f7d71c9c5f5ba99a1
Todd Matthews <matthews.todd@gmail.com>
2010-12-07 17:00:38 -0500

    Added new layout to main application layout. Added check in welcome controller for linkedin token

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,11 +1,10 @@
-
 class WelcomeController < ApplicationController
   def index
-
+    if session[:atoken]
+      linkedin = Linkedin.first
+      client = LinkedIn::Client.new(linkedin.api_key, linkedin.secret_key)
+      client.authorize_from_access(session[:atoken], session[:asecret])      
+      @profile = client.profile(:fields => %w(id first-name last-name industry headline site-standard-profile-request))
+    end
   end
-    
-  def test
-    
-  end
-
 end
