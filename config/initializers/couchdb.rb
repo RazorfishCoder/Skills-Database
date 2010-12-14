31500c93ef8f85ba180580ff539d110cfbf0839e
Martin Jacobs <martin.jacobs@gmail.com>
2010-12-14 17:53:01 -0500

    Allowed for heroku database connection

--- config/initializers/couchdb.rb
+++ config/initializers/couchdb.rb
@@ -1,3 +1,10 @@
 require 'couchrest/model'
-CouchServer = CouchRest::Server.new # defaults to localhost:5984
-CouchServer.default_database = "skillsdb-#{Rails.env}"
+#CouchServer = CouchRest::Server.new # defaults to localhost:5984
+# Define which CouchDB instance to use.
+# Creates the database if it does not exist already.
+if ENV['CLOUDANT_URL']
+  CouchServer = CouchRest.database!( ENV['CLOUDANT_URL'] + ENV['APP_NAME'] )
+else
+  CouchRest::Server.new # defaults to localhost:5984
+  CouchServer.default_database = "skillsdb-#{Rails.env}"
+end
