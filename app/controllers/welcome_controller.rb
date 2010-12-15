8a7faf879545df69260ec8629b5d7dd4f536f606
Martin Jacobs <martin.jacobs@gmail.com>
2010-12-14 21:29:24 -0500

    Allowed for heroku database connection

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,12 +1,11 @@
 class WelcomeController < ApplicationController
   def index
-    if session[:atoken]
-      ## check if the user is already logged in.
+    if session[:atoken]  ## check if the user is already logged in.
       linkedin = Linkedin.first
       client = LinkedIn::Client.new(linkedin.api_key, linkedin.secret_key)
       client.authorize_from_access(session[:atoken], session[:asecret])      
       profile = client.profile(:fields => %w(id first-name last-name industry headline site-standard-profile-request))
+      @employee = Employee.find_by_linkedin_id(profile.id)
     end
-    @employee = Employee.find_by_linkedin_id(profile.id)
   end
 end
