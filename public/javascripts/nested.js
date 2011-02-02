6da75ece4ab9836f86582d6167e1ec1b4072c5a7
Alejandro Valle <avalle@sinaptica.es>
2011-02-02 18:01:07 -0300

    almost ended

--- public/javascripts/nested.js
+++ public/javascripts/nested.js
@@ -1,21 +1,29 @@
-$(function() {
-  $('form a.add_child').click(function() {
-    var assoc   = $(this).attr('data-association');
-    var content = $('#' + assoc + '_fields_template').html();
-    var regexp  = new RegExp('new_' + assoc, 'g');
+$(document).ready(function(){
+  $('#add_skill').live('click',function() {
+    var content = $('#skill_tag_fields_template').html();
     var new_id  = new Date().getTime();
-
-    $(this).parent().before(content.replace(regexp, new_id));
-    return false;
+    $('#skill_tags').before(content.replace('run_time_name', new_id));
+        alert('so siome');
+      return false;
   });
 
-  $('form a.remove_child').live('click', function() {
-    var hidden_field = $(this).prev('input[type=hidden]')[0];
-    if(hidden_field) {
-      hidden_field.value = '1';
-    }
-    $(this).parents('.fields').hide();
-    return false;
-  });
+//  $('form a.add_child').click(function() {
+//    var assoc   = $(this).attr('data-association');
+//    var content = $('#' + assoc + '_fields_template').html();
+//    var regexp  = new RegExp('new_' + assoc, 'g');
+//    var new_id  = new Date().getTime();
+
+//    $(this).parent().before(content.replace(regexp, new_id));
+//    return false;
+//  });
+
+//  $('form a.remove_child').live('click', function() {
+//    var hidden_field = $(this).prev('input[type=hidden]')[0];
+//    if(hidden_field) {
+//      hidden_field.value = '1';
+//    }
+//    $(this).parents('.fields').hide();
+//    return false;
+//  });
 });
 
