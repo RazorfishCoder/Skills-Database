82fa97a7d46d664446340647f7dff898fa207aec
Todd Matthews <matthews.todd@gmail.com>
2011-02-11 10:31:19 -0500

    Added initial rspec setup to create test database setup/teardown as well as some base fixtures and employee_spec classes.
    Signed-off-by: Todd Matthews <matthews.todd@gmail.com>

---
+++ spec/models/employee_spec.rb
@@ -0,0 +1,37 @@
+require File.expand_path("../../spec_helper", __FILE__)
+require File.join(FIXTURE_PATH, 'base_fixture')
+
+describe Employee do
+  
+  before(:each) do
+    @obj = WithDefaultValues.new
+  end
+  
+  # This was an initial test to see how couchrest model was working...can probably be removed.  
+  describe "instance database connection" do
+      it "should use the default database" do
+        @obj.database.name.should == "skillsdb-#{Rails.env}"
+      end
+
+      it "should override the default db" do
+        @obj.database = CouchServer.database!('couchrest-extendedmodel-test')
+        @obj.database.name.should == 'couchrest-extendedmodel-test'
+        @obj.database.delete!
+      end
+  end
+  
+  describe "a new model" do
+      it "should be a new document" do
+        @obj.rev.should be_nil
+        @obj.should be_new
+        @obj.should be_new_document
+        @obj.should be_new_record
+      end
+
+      it "should not failed on a nil value in argument" do
+        @obj = WithDefaultValues.new(nil)
+        @obj.should_not be_nil
+      end
+    end
+    
+end
